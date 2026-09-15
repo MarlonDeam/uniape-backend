9b7adaec2e85357f304d50c62f1269aa239599df
Marlon <marlonnobre.dev@gmail.com>
2026-09-15 12:34:57 -0300

    Configura CORS no backend

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import usuarioRoutes from "./routes/usuario.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
